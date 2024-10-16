4d1e4fa82ff5e522bdd86d3a8260357317f4430a
megavardhini1958@gmail.com <megavardhini1958@gmail.com>
2024-10-16 23:39:42 +0530

    commit

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
-//import './Product.css'
+
 
 const ProductCard = ({ product }) => {
   const { user, cartItems, setCartItems } = useAuth();
